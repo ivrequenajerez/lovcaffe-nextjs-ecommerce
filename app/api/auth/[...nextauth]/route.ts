26cecd19838bcf033e37204fb5916d7395b69561
Ian Valentino <130348417+ivrequenajerez@users.noreply.github.com>
2025-08-17 20:28:53 +0200

    chore: base setup con header, hero y auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,9 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-});
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
+
+export const dynamic = "force-dynamic";
+
+export const runtime = "nodejs";
---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+// lib/auth.ts
+import GitHubProvider from "next-auth/providers/github";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
